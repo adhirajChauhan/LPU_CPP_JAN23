db5b27ad4438dd20b2f92ad71f67f1c4365bca76
adhirajChauhan <adhirajsingh0105@gmail.com>
2023-01-28 21:26:47 +0530

    LL problem, stacks

--- myData/DSA/LinkedList/doubly_LL.cpp
+++ myData/DSA/LinkedList/doubly_LL.cpp
@@ -59,7 +59,7 @@ void deletionAtHead(Node *&head)
 
 void deletion(Node *&head, int pos)
 {
-    if(pos==1)
+    if (pos == 1)
     {
         deletionAtHead(head);
         return;
@@ -84,19 +84,78 @@ void deletion(Node *&head, int pos)
     delete temp;
 }
 
-//Find the length of dLL
+// Find the length of dLL
 int length(Node *&head)
 {
     int len = 0;
     Node *temp = head;
 
-    while(temp!=NULL){
+    while (temp != NULL)
+    {
         temp = temp->next;
         len++;
     }
 
     return len;
+}
 
+void intersect(Node *&head1, Node *&head2, int pos)
+{
+    Node *temp1 = head1;
+    pos--;
+    while(pos--){
+        temp1=temp1->next;
+    }
+    Node *temp2=head2;
+    while(temp2->next!=NULL){
+        temp2 = temp2->next;
+    }
+    temp2->next=temp1;
+}
+
+int intersection(Node *head1, Node *head2)
+{
+    int l1 = length(head1);
+    int l2 = length(head2);
+
+    int d = 0;
+    Node *ptr1;
+    Node *ptr2;
+
+    if (l1 > l2)
+    {
+        d = l1 - l2;
+        ptr1 = head1;
+        ptr2 = head2;
+    }
+    else
+    {
+        d = l2 - l1;
+        ptr1 = head2;
+        ptr2 = head1;
+    }
+
+    while (d)
+    {
+        ptr1 = ptr1->next;
+        if (ptr1 == NULL)
+        {
+            return -1;
+        }
+        d--;
+    }
+
+    while (ptr1 != NULL && ptr2 != NULL)
+    {
+        if (ptr1 == ptr2)
+        {
+            return ptr1->data;
+        }
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+
+    return -1;
 }
 
 void display(Node *head)
@@ -113,15 +172,34 @@ void display(Node *head)
 
 int main()
 {
-    Node *head = NULL;
-    insertAtTail(head, 1);
-    insertAtTail(head, 2);
-    insertAtTail(head, 3);
-    display(head);
-    insertAtHead(head, 4);
-    display(head);
-    deletion(head, 2);
-    display(head);
-    cout << length(head) << endl;
-    display(head);
+    Node *head1 = NULL;
+    Node *head2 = NULL;
+
+    // insertAtTail(head, 1);
+    // insertAtTail(head, 2);
+    // insertAtTail(head, 3);
+    // display(head);
+    // insertAtHead(head, 4);
+    // display(head);
+    // deletion(head, 2);
+    // display(head);
+    // cout << length(head) << endl;
+    // display(head);
+
+    insertAtTail(head1,1);
+    insertAtTail(head1,2);
+    insertAtTail(head1,3);
+    insertAtTail(head1,4);
+    insertAtTail(head2,5);
+    insertAtTail(head2,6);
+    insertAtTail(head2,7);
+
+    intersect(head1,head2,3);
+
+    display(head1);
+    display(head2);
+
+    cout << "Intersecting at : " << intersection(head1, head2) << endl;
+
+
 }
